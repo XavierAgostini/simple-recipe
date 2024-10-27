cb1df4a61ced8f6c8ddf3e36cecddff976523b37
Xavier Agostini <xavier.agostini@runway.com>
2024-10-27 19:28:43 -0400

    init

---
+++ src/app/recipes/[slug]/page.tsx
@@ -0,0 +1,34 @@
+// pages/recipe/[slug].tsx
+import recipes from '../../../data/recipes.json';
+
+const RecipePage = async ({ params }: { params: { slug: string } }) => {
+
+  const { slug } = await params;
+  const recipe = recipes[slug];
+
+  if (!recipe) {
+    return <p>Recipe not found</p>;
+  }
+
+  return (
+  <div>
+    <h1>{recipe.title}</h1>
+    <p><strong>Meat Type:</strong> {recipe.meatType}</p>
+    <p><strong>Animal:</strong> {recipe.animal}</p>
+    <p><strong>Cut:</strong> {recipe.cut}</p>
+    <p><strong>Cooking Time:</strong> {recipe.cookingTime}</p>
+    <p>
+      <strong>Temperature:</strong> {recipe.temperature.imperial} / {recipe.temperature.metric}
+    </p>
+    <p><strong>Kitchen Aid:</strong> {recipe.kitchenAid}</p>
+    <h2>Instructions</h2>
+    <ol>
+      {recipe.instructions.map((instruction, index) => (
+        <li key={index}>{instruction}</li>
+      ))}
+    </ol>
+  </div>
+  )
+}
+
+export default RecipePage;
